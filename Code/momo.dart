977c2f26ce19278f8084ef582e8d020e24f23c07
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2022-07-14 05:49:34 +0100

    Update momo.dart

--- Code/momo.dart
+++ Code/momo.dart
@@ -1,1 +1,22 @@
-kikk
+
+import 'package:flutter/material.dart'
+
+void main() {
+runApp(MyApp());
+}
+
+class myApp extends StateLessWidget{
+  @override
+  Widget build(Buildcontext context){
+    return MaterialApp(
+      title: "momouniverse" ,
+      theme: Themedata(primarySwatch: colors.yellow),
+      home: MyHomePage()
+      );
+    }
+}
+
+class MyHomePage extends StatefullWidget{
+  @override 
+    _MyHomePageState creatState()=> _MyHomePageState();
+}
